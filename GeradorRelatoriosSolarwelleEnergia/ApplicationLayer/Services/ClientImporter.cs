cc0a30563f5f96d9f71777e4528f43458b37bc7c
rafaelpmaio <rafaelp91@gmail.com>
2025-08-18 17:43:53 -0300

    incluindo pasta AssetsReadme com arquivos que podem ser compartilhados

--- GeradorRelatoriosSolarwelleEnergia/ApplicationLayer/Services/ClientImporter.cs
+++ GeradorRelatoriosSolarwelleEnergia/ApplicationLayer/Services/ClientImporter.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeradorRelatoriosSolarwelleEnergia.Infrastructure.Database;
+using GeradorRelatoriosSolarwelleEnergia.Infrastructure.Readers;
 
-namespace GeradorRelatoriosSolarwelleEnergia
+namespace GeradorRelatoriosSolarwelleEnergia.ApplicationLayer.Services
 {
     internal class ClientImporter
     {
+        private readonly ClientRepository _repository = new();
+        private readonly ClientExcelReader _reader = new();
+
+        public void ImportFromExcelToDb(string filePath)
+        {
+            var clients = _reader.ReadClients(filePath);
+            foreach (var client in clients)
+            {
+                _repository.Insert(client);
+            }
+            Console.WriteLine("Importação concluída com sucesso");
+        }
     }
 }
